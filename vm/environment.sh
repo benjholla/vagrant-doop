e9315bfbe2f4196d3500fbae8f3c9c44c28dd727
Ben Holland <benjholla@gmail.com>
2015-12-22 15:29:07 -0600

    fixed environment variable paths

--- vm/environment.sh
+++ vm/environment.sh
@@ -9,12 +9,11 @@ LB_VERSION=3.10.29
 export LB_MEM="3G"
 export LB_MEM_NOWARN=1
 export LB_PAGER_FORCE_START=1
-export LOGICBLOX_HOME=${dir}/logicblox-$LB_VERSION
+export LOGICBLOX_HOME=/opt/logicblox-$LB_VERSION
 export PATH=${dir}/doop/tools/bin:$LOGICBLOX_HOME/bin:$PATH
 
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 export JRE_LIB=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/
-export LOGICBLOX_HOME=${dir}/logicblox-$LB_VERSION/logicblox
 
 export DOOP_HOME=${dir}/doop/
 export DOOP_JRE_LIB=${dir}/doop/benchmarks/JREs/
