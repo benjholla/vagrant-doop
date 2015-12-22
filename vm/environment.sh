9abd40958c486ec4f612afda6d3b66815e1bf9c9
Ben Holland <benjholla@gmail.com>
2015-12-22 16:24:23 -0600

    adding doop tools

--- vm/environment.sh
+++ vm/environment.sh
@@ -4,17 +4,25 @@
 echo "Setting LogicBlox and DOOP environment..."
 dir=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
 
+# logicblox version
 LB_VERSION=3.10.29
 
+# setup logicblox environment
 export LB_MEM="3G"
 export LB_MEM_NOWARN=1
 export LB_PAGER_FORCE_START=1
 export LOGICBLOX_HOME=/opt/logicblox-$LB_VERSION
 export PATH=${dir}/doop/tools/bin:$LOGICBLOX_HOME/bin:$PATH
 
+# setup java environment
 export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 export JRE_LIB=/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/
 
+# setup doop environment
 export DOOP_HOME=${dir}/doop/
 export DOOP_JRE_LIB=${dir}/doop/benchmarks/JREs/
 export DOOP_EXTERNALS=${dir}/doop/benchmarks/JREs
+
+# add doop tools to environment path
+PATH=$PATH:/vagrant/doop-tools/bin
+export PATH
